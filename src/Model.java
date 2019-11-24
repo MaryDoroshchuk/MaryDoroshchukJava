d3448ec600eb4d44e1579f8c9ca856438a690e5b
MaryDoroshchuk <mary.doroshchuk@gmail.com>
2019-11-25 01:21:01 +0200

    My_first_commit

---
+++ src/Model.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+    int BASE_MAX_VALUE = 10;
+    int BASE_MIN_VALUE = 0;
+
+    private int maxValue = BASE_MAX_VALUE;
+    private int minValue = BASE_MIN_VALUE;
+    private int randomValue;
+    private List<Integer> yourHistory = new ArrayList<Integer>();
+
+    public void setRandomValue () {
+        randomValue = (int) Math.ceil(Math.random()*(maxValue-minValue-1) + minValue);
+    }
+
+    public boolean checkValue(int userValue) {
+        yourHistory.add(userValue);
+        if (userValue == randomValue) {
+            return true;
+        } else if (userValue > randomValue) {
+            maxValue = userValue - 1;
+            return false;
+        } else {
+            minValue = userValue + 1;
+            return false;
+        }
+    }
+
+    public void setBaseValue (int maxValue, int minValue){
+    this.maxValue = maxValue;
+    this.minValue = minValue;
+    }
+    public int getRandomValue() {
+        return randomValue;
+    }
+    public int getMaxValue() {
+        return maxValue;
+    }
+    public int getMinValue() {
+        return minValue;
+    }
+    public List<Integer> getYourHistory() {
+        return yourHistory;
+    }
+}
+
+
---
+++ src/View.java
@@ -0,0 +1,11 @@
+public class View {
+    public static final String INPUT_INT_DATA = "Input value: ";
+    public static final String TO = " to ";
+    public static final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please! ";
+    public static final String CONGRATULATION = "YOU WIN!!! CONGRATULATION! Guessed number: ";
+    public static final String YOUR_HISTORY = "Your history: ";
+
+    public void printMessage (String message) {
+        System.out.println(message);
+    }
+}
